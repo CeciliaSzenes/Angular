221361441d37118d6949194235f35e53ceaa0922
Cecilia Szenes <cecilia.szenes@gmail.com>
2020-04-30 15:30:42 +0200

    First 4 steps of tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Joker' },
+  { id: 12, name: 'Batman' },
+  { id: 13, name: 'TwoFace' },
+  { id: 14, name: 'WonderWoman' },
+  { id: 15, name: 'Hulk' },
+  { id: 16, name: 'Robin' },
+  { id: 17, name: 'Harley Quinn' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
